99b6d567943c002dced9ea61c4ac990d63a22533
Abdifatah Mohamed <mofaatah@gmail.com>
2022-09-05 17:57:34 -0500

    Added some styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -40,7 +40,15 @@ const Home = () => {
     }
   };
   return (
-    <div className="m-t">
+    <div className="home-content">
+      <h1>A brief description about this application</h1>
+        <p className="text">This full-stack web application that uses <strong>Node.js</strong> with <strong>Express</strong>, <strong>MySQL</strong>, and <strong>React.js</strong>.
+            This project is great intro incorporating the front-end to the backend and adding database to it.
+            Through the teams name and stadium, we can <strong>CREATE/INSERT</strong> new team information into the table storing data to the mysql database.
+            We can also <strong>SEARCH</strong> by the team name using the search input -- if the team name doesn't exist it will show "No data".
+            We can also <strong>UPDATE</strong> existing team name by just clicking the edit button.
+            Finally, we can <strong>DELETE</strong> any exiting team information from the database by just using delete button.
+        </p>
       <Link to="/addTeam">
         <button className="btn btn-contact">Add Team</button>
       </Link>
